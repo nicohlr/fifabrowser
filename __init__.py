d52fdd02fdc3c064c0aa319b91280c523f5bba3b
Nicolas <nicolas.houlier@gmail.com>
2019-05-07 23:37:54 +0200

    design homepage

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
 app.debug = True
+app.static_folder = 'static'
 
 secret = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'utils/secret.txt'))
 app.secret_key = secret.readline()[:-1]
@@ -10,7 +11,22 @@ app.secret_key = secret.readline()[:-1]
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 if __name__ == '__main__':
