f1f4c8fc4ae88562743af1c079b1224f51533edc
Nicolas <nicolas.houlier@gmail.com>
2019-05-07 22:34:20 +0200

    safe adding of secret key

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 app.debug = True
+
+secret = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'utils/secret.txt'))
+app.secret_key = secret.readline()[:-1]
 
 
 @app.route('/')
