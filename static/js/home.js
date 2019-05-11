8d2b3036e2f7f2714c9167cdc3eae324b8d8f0ed
Nicolas <nicolas.houlier@gmail.com>
2019-05-11 23:24:03 +0200

    minor design improvement

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,7 @@ $('input[class="search_input"]').autoComplete({
     minChars: 1,
     source: function(term, suggest){
         term = term.toLowerCase();
-        var choices = [['Australia'], ['Austria'], ['Brasil']];
+        var choices = [['Australia', 'test'], ['Austria'], ['Brasil']];
         var suggestions = [];
         for (i=0;i<choices.length;i++)
             if (~(choices[i][0]+' '+choices[i][1]).toLowerCase().indexOf(term)) suggestions.push(choices[i]);
