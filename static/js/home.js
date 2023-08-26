bd6b610efa27c14abbf004f53f860c5a106bfaee
Nicolas Houlier <nicolas.houlier@gmail.com>
2023-08-26 09:22:20 +0200

    Performances improvement (AJAX + parquet)

--- static/js/home.js
+++ static/js/home.js
@@ -1,34 +1,23 @@
 $(document).ready(function(){
-    input_pattern = ''
-    players_list.forEach(function (item, index) {
-      input_pattern = input_pattern + '|' + item[0];
-    });
-
-    var player_field = document.getElementsByClassName("form_player")
-    player_field[0].setAttribute("required", "required");
-    player_field[0].setAttribute("pattern", input_pattern);
-
-    player_field[0].oninvalid = function(event) {
-        event.target.setCustomValidity('Player must be chosen among the list of players');
-    }
-
     $('input[class="form_player"]').autoComplete({
         minChars: 2,
-        delay: 100,
+        delay: 100,  // This delay is for the built-in delay of the autoComplete plugin itself
         source: function(term, suggest){
             term = term.toLowerCase();
-            var choices = players_list;
-            var suggestions = [];
-            for (i=0;i<choices.length;i++)
-                if (~(choices[i][0]+' '+choices[i][1]).toLowerCase().indexOf(term)) suggestions.push(choices[i]);
-            suggest(suggestions);
+            $.ajax({
+                url: "/autocomplete",  // endpoint to fetch the autocomplete suggestions
+                data: { query: term },
+                success: function(response) {
+                    suggest(response);
+                }
+            });
         },
-        renderItem: function (item, search){
+        renderItem: function(item, search) {
             search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
             var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-            return '<div class="autocomplete-suggestion" data-langname="'+item[0]+'" data-lang="'+item[1]+'" data-val="'+search+'">'+item[0].replace(re, "<b>$1</b>")+'</div>';
+            return '<div class="autocomplete-suggestion" data-langname="' + item[0] + '" data-lang="' + item[1] + '" data-val="' + search + '">' + item[0].replace(re, "<b>$1</b>") + '</div>';
         },
-        onSelect: function(e, term, item){
+        onSelect: function(e, term, item) {
             e.preventDefault();
             $('input[class="form_player"]').val(item.data('langname'));
         },
--- utils.py
+++ utils.py
@@ -1,40 +1,29 @@
-import pandas as pd
 from unidecode import unidecode
 
 
 def clean_playername(name):
-    return unidecode(name.replace('.', '').replace(' ', '_').upper())
+    return unidecode(name.replace(".", "").replace(" ", "_").upper())
 
 
-def get_player_list():
-    df = pd.read_csv('data/data.csv')
-    record = df[['Name', 'Flag']].to_dict('record')
+def get_player_list(df):
+    record = df[["Name", "Flag"]].to_dict("records")
     return [list(r.values()) for r in record]
 
 
-def get_player_pattern():
-    df = pd.read_csv('data/data.csv')
-    return (df['Name'].to_string(index=False)
-                      .replace('  ', '')
-                      .replace('\n ', '|')
-                      .replace('\n', '|'))
+def get_player_pattern(df):
+    return (
+        df["Name"]
+        .to_string(index=False)
+        .replace("  ", "")
+        .replace("\n ", "|")
+        .replace("\n", "|")
+    )
 
 
-def get_player_attributes(playername):
+def get_player_attributes(df, playername):
+    temp_wr = df["Work Rate"].str.split("/", expand=True)
+    df["Work Rate"] = temp_wr[0] + " / " + temp_wr[1]
 
-    cols_to_drop = [
-        'Value', 'Wage', 'Special', 'Joined', 'Loaned From', 'LS', 'ST', 'RS',
-        'LW', 'LF', 'CF', 'RF', 'RW', 'LAM', 'CAM', 'RAM', 'LM', 'LCM', 'RDM',
-        'Contract Valid Until', 'CM', 'RCM', 'RM', 'LWB', 'LDM', 'CDM', 'RWB',
-        'LB', 'LCB', 'CB', 'RCB', 'RB', 'Photo', 'Club Logo'
-    ]
+    df["NameFMT"] = df["Name"].apply(clean_playername)
 
-    df = pd.read_csv('data/data.csv')
-    df = df[df.drop(cols_to_drop, 1).columns.tolist()[2:-1]]
-
-    temp_wr = df['Work Rate'].str.split('/', expand=True)
-    df['Work Rate'] = temp_wr[0] + ' / ' + temp_wr[1]
-
-    df['NameFMT'] = df['Name'].apply(clean_playername)
-
-    return df[df['NameFMT'] == playername].to_dict('records')[0]
+    return df[df["NameFMT"] == playername].to_dict("records")[0]
